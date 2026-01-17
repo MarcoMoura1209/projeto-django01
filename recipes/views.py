9de87ff646984712f92cfedee7f24f1a26e275d1
Marco Moura <marcomoura27052009@gmail.com>
2026-01-17 16:35:50 -0300

    Add home and ajuda views, templates for recipes, and update settings

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Esta é a pagina inicial')
+    return render(request, 'recipes/home.html')
 
 
 def ajuda(request):
