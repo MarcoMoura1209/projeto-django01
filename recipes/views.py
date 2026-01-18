9c1d3035977ee6966380747bf479012a9b5fcd67
Marco Moura <marcomoura27052009@gmail.com>
2026-01-18 16:14:02 -0300

    Remove help view and its URL path from recipes app

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 # Create your views here.
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-def help(request):
-    return HttpResponse('Pagina de ajuda')
-
