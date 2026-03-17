42895f80d202ec9bb2f2e6909b4135962eaabde1
Marco Moura <marcomoura27052009@gmail.com>
2026-03-17 08:28:48 -0300

    Add media and static file settings, update Recipe model with __str__ method, and register RecipeAdmin in admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category
+from .models import Category, Recipe
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
 
 
 class CategoryAdmin(admin.ModelAdmin):
