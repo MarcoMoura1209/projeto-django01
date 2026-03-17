04b88395024ac3bdb143f3b6df0ea0e54557d278
Marco Moura <marcomoura27052009@gmail.com>
2026-03-17 17:00:31 -0300

    Add category view and template to display recipes by category

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from utils.recipe.factory import make_recipe
+
 from .models import Recipe
-# Create your views here.
-# from django.http import HttpResponse
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True
+        ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True
+    ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
@@ -15,5 +25,5 @@ def home(request):
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-views.html', context={
         'recipe': make_recipe(),
-        'is_detail': True,
+        'is_detail_page': True,
     })
