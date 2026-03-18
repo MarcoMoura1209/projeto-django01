208b7215703b0fde602f815e20355635f750e22e
Marco Moura <marcomoura27052009@gmail.com>
2026-03-18 08:33:07 -0300

    Refactor category view to use get_list_or_404 for better error handling

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from utils.recipe.factory import make_recipe
 
 from recipes.models import Recipe
@@ -14,12 +14,13 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True
-    ).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    )
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'titleCategory': f'{recipes.first().category.name} - Category | '
+        'titleCategory': f'{recipes[0].category.name} - Category | '
     })
 
 
