b62eb61fbccac6dcc15fce2b19b1619a1ae12b6d
Marco Moura <marcomoura27052009@gmail.com>
2026-03-18 19:10:09 -0300

    Alteration in indentation in views.py

--- recipes/views.py
+++ recipes/views.py
@@ -15,7 +15,8 @@ def home(request):
 
 def category(request, category_id):
     recipes = get_list_or_404(
-        Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+        Recipe.objects.filter(category__id=category_id,
+                              is_published=True).order_by('-id')
     )
 
     return render(request, 'recipes/pages/category.html', context={
