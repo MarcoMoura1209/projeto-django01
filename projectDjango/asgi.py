9c1cb3051b5e4b63a9720b6836a35b9a41aae6f8
Marco Moura <marcomoura27052009@gmail.com>
2026-03-23 17:29:14 -0300

    Fix project name casing in settings and update related files; add initial ASGI, WSGI, and URL configurations

--- projectDjango/asgi.py
+++ projectDjango/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for projetcDjango project.
+ASGI config for projectDjango project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetcDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectDjango.settings')
 
 application = get_asgi_application()
--- projectDjango/wsgi.py
+++ projectDjango/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projetcDjango project.
+WSGI config for projectDjango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetcDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectDjango.settings')
 
 application = get_wsgi_application()
