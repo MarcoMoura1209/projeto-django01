52212f1786f282e9ce4f1587d886e58ae9b186fd
Marco Moura <marcomoura27052009@gmail.com>
2026-03-23 16:08:02 -0300

    Initialize projetcDjango project with basic settings, URL routing, and WSGI/ASGI configurations

--- projetcDjango/asgi.py
+++ projetcDjango/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for projetoDjango project.
+ASGI config for projetcDjango project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetoDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetcDjango.settings')
 
 application = get_asgi_application()
--- projetcDjango/wsgi.py
+++ projetcDjango/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projetoDjango project.
+WSGI config for projetcDjango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetoDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetcDjango.settings')
 
 application = get_wsgi_application()
