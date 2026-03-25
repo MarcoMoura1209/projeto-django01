39b722984a8628f89f73b3e81482d901303597c3
Marco Moura <marcomoura27052009@gmail.com>
2026-03-25 14:52:46 -0300

    Add tests for recipe home and recipe URLs

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,7 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class RecipeURLS_Test(TestCase):
 
     def test_recipe_home_url_is_correct(self):
-        ...
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
+
+    def test_recipe_recipe_url_is_correct(self):
+        recipe_url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(recipe_url, '/recipes/1/')
