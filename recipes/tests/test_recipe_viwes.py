53f1fe539831623196ce1f94d11d8bed3438976a
Marco Moura <marcomoura27052009@gmail.com>
2026-04-14 14:36:29 -0300

    Add test for recipe home template loading recipes

--- recipes/tests/test_recipe_viwes.py
+++ recipes/tests/test_recipe_viwes.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
+from recipes.models import Category, Recipe, User
 
 
 class test_Recipe_Viwes(TestCase):
@@ -41,3 +43,28 @@ class test_Recipe_Viwes(TestCase):
             reverse('recipes:recipe', kwargs={'id': 1000})
         )
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )
+        assert 1 == 1
