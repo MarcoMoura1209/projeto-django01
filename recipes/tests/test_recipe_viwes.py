a7919e2699e6d614a9c0bbb7928e2b44df8f2fd1
Marco Moura <marcomoura27052009@gmail.com>
2026-04-14 13:45:15 -0300

    Add additional tests for recipe home view functionality

--- recipes/tests/test_recipe_viwes.py
+++ recipes/tests/test_recipe_viwes.py
@@ -4,6 +4,7 @@ from recipes import views
 
 
 class test_Recipe_Viwes(TestCase):
+
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
@@ -19,3 +20,11 @@ class test_Recipe_Viwes(TestCase):
             reverse('recipes:recipe', kwargs={'id': 1})
         )
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
