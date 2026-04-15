fdf10951f058f043907cf184925cd0b43646efa7
Marco Moura <marcomoura27052009@gmail.com>
2026-04-15 14:31:02 -0300

    Add base test setup for recipe model and enhance category view tests

--- recipes/tests/test_recipe_viwes.py
+++ recipes/tests/test_recipe_viwes.py
@@ -2,10 +2,10 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
-from recipes.models import Category, Recipe, User
+from .test_recipe_base import TestRecipeBase
 
 
-class test_Recipe_Viwes(TestCase):
+class test_Recipe_Viwes(TestRecipeBase):
 
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
@@ -27,6 +27,12 @@ class test_Recipe_Viwes(TestCase):
         response = self.client.get(reverse('recipes:home'))
         self.assertEqual(response.status_code, 200)
 
+    def test_recipe_category_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
+
     def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
@@ -37,7 +43,6 @@ class test_Recipe_Viwes(TestCase):
         )
         self.assertEqual(response.status_code, 404)
 
-
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(
             reverse('recipes:recipe', kwargs={'id': 1000})
@@ -45,29 +50,6 @@ class test_Recipe_Viwes(TestCase):
         self.assertEqual(response.status_code, 404)
 
     def test_recipe_home_template_loads_recipes(self):
-        category = Category.objects.create(name='Category')
-        author = User.objects.create_user(
-            first_name='user',
-            last_name='name',
-            username='username',
-            password='123456',
-            email='username@email.com',
-        )
-        recipe = Recipe.objects.create(
-            category=category,
-            author=author,
-            title='Recipe Title',
-            description='Recipe Description',
-            slug='recipe-slug',
-            preparation_time=10,
-            preparation_time_unit='Minutos',
-            servings=5,
-            servings_unit='Porções',
-            preparation_steps='Recipe Preparation Steps',
-            preparation_steps_is_html=False,
-            is_published=True,
-        )
-
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
